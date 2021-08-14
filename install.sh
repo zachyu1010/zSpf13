b51f216e8afa11d3e10e1cd5be03e994a4b80866
zachary_yu <zachary.yu@kneron.us>
2021-08-14 21:59:16 +0800

    add temperay token

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # spf13 website: http://vim.spf13.com/
 # https://github.com/spf13/spf13-vim
+# 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
 
 # install spf13  packages
  curl http://j.mp/spf13-vim3 -L -o - | sh
