b3147c0e29c00fb9cb9b7943c7ea5a408d5e49bf
zachary.yu <zachary.yu@kneron.us>
2021-08-14 02:19:11 -0700

    my spf13 vim ide

---
+++ install.sh
@@ -0,0 +1,15 @@
+# spf13 website: http://vim.spf13.com/
+# https://github.com/spf13/spf13-vim
+
+# install spf13  packages
+# curl http://j.mp/spf13-vim3 -L -o - | sh
+
+# set link for zach's customization
+ln -s ~/zSpf13/.vimrc.before.local ~/.vimrc.before.local
+ln -s ~/zSpf13/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/zSpf13/.vimrc.local ~/.vimrc.local
+ln -s ~/zSpf13/plugin/ ~/.vim/plugin
+
+
+
+
