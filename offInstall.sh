7c2b756c747f482641fd68dc39717ca9118fc31e
zachary <zachary.yu@kneron.us>
2021-12-14 01:04:36 -0800

    utility, offInstall.sh, to build the symbolic links for vim settings

--- offInstall.sh
+++ offInstall.sh
@@ -6,7 +6,10 @@
  #curl http://j.mp/spf13-vim3 -L -o - | sh
 
 # set link for zach's customization
+ln -sf ~/.spf13-vim-3/.vimrc ~/.vimrc
+ln -sf ~/.spf13-vim-3/.vimrc.before ~/.vimrc.before
 ln -sf ~/zSpf13/.vimrc.before.local ~/.vimrc.before.local
+ln -sf ~/.spf13-vim-3/.vimrc.bundles ~/.vimrc.bundles
 ln -sf ~/zSpf13/.vimrc.bundles.local ~/.vimrc.bundles.local
 ln -sf ~/zSpf13/.vimrc.local ~/.vimrc.local
 ln -sf ~/zSpf13/plugin/ ~/.vim/plugin
